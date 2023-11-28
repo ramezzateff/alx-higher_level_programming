309ba5ca1d4b1f97efd651a95d24165d4b58074e
ramezzateff <ramezatef62@gmail.com>
2023-11-29 00:51:28 +0200

    aa

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
-#!/bin/bash/python3
+#!/usr/bin/python3
 for letter in range(97, 123):
-    print("{}".format(chr(letter)), end="")
+     if chr(letter) != 'q' and chr(letter) != 'e':
+         print("{}".format(chr(letter)), end="")
